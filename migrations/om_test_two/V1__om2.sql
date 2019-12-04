129716d4fbee619cfa358b49ba478f62d4491c3a
omshanti0805 <54437718+omshanti0805@users.noreply.github.com>
2019-12-04 15:11:31 +0530

    Add files via upload

--- migrations/om_test_two/V1__om2.sql
+++ migrations/om_test_two/V1__om2.sql
@@ -1,4 +1,4 @@
-create table tutorials_tbl(
+create table hariom(
    tutorial_id INT NOT NULL AUTO_INCREMENT,
    tutorial_title VARCHAR(100) NOT NULL,
    tutorial_author VARCHAR(40) NOT NULL,
--- migrations/om_test_two/V2__om22.sql
+++ migrations/om_test_two/V2__om22.sql
@@ -1,4 +1,4 @@
-create table shanti(
+create table shantiom(
    tutorial_id INT NOT NULL AUTO_INCREMENT,
    tutorial_title VARCHAR(100) NOT NULL,
    tutorial_author VARCHAR(40) NOT NULL,
